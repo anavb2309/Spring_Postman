0b32e827925d56e60b5ab8c28731118a9661d7da
anavb2309 <anavalenciaburbano@gmail.com>
2024-12-11 15:06:20 -0500

    spring_post1

--- src/main/java/com/interivalle/spring3/controlador/UsuarioControler.java
+++ src/main/java/com/interivalle/spring3/controlador/UsuarioControler.java
@@ -19,7 +19,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 /**
  *
- * @author Marysela Velasco
+ * Cotizador Interivalle
+ * 
  */
 
 @CrossOrigin(origins = "http://localhost:3000")
